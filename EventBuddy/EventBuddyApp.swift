542ad398e2d312242e385c61098203d28427e98a
Harry Ng <harryworld@gmail.com>
2025-05-16 10:11:33 +0800

    Add initial Xcode project

---
+++ EventBuddy/EventBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventBuddyApp.swift
+//  EventBuddy
+//
+//  Created by Harry Ng on 16/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct EventBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
